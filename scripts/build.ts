3a4fe9686c36444097490a76937c63eae10d1669
patrickjm <patrick@moriarty.dev>
2023-11-12 18:49:31 -0600

    Fix Cjs support

--- scripts/build.ts
+++ scripts/build.ts
@@ -6,7 +6,23 @@ import { build as esbuild } from "esbuild";
 const srcPath = path.join(process.cwd(), "src");
 const buildPath = path.join(process.cwd(), "build");
 
-async function buildFile(filePath: string) {
+async function bundleCjs() {
+  const filePath = "index.ts";
+  await esbuild({
+    platform: "node",
+    target: "node18",
+    format: "cjs",
+    outExtension: { ".js": ".cjs" },
+    nodePaths: [srcPath],
+    sourcemap: true,
+    external: [],
+    bundle: true,
+    entryPoints: [path.join(srcPath, filePath)],
+    outdir: path.join(buildPath, path.dirname(filePath)),
+  })
+}
+
+async function buildFileEsm(filePath: string) {
   await esbuild({
     platform: "node",
     target: "node18",
@@ -18,18 +34,6 @@ async function buildFile(filePath: string) {
     entryPoints: [path.join(srcPath, filePath)],
     outdir: path.join(buildPath, path.dirname(filePath)),
   });
-
-  await esbuild({
-    platform: "node",
-    target: "node18",
-    format: "cjs",
-    outExtension: { ".js": ".cjs" },
-    nodePaths: [srcPath],
-    sourcemap: true,
-    external: [],
-    entryPoints: [path.join(srcPath, filePath)],
-    outdir: path.join(buildPath, path.dirname(filePath)),
-  })
 }
 
 async function build({ includeTests = false }: { includeTests?: boolean }) {
@@ -44,8 +48,10 @@ async function build({ includeTests = false }: { includeTests?: boolean }) {
       throw new TypeError("Unexpected file type");
     }
 
-    await buildFile(filePath);
+    await buildFileEsm(filePath);
   }
+
+  await bundleCjs();
 }
 
 if (import.meta.url.startsWith("file:")) {
